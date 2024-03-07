10de960d67650ccbfb5cf9dea82b528fca0524e7
Sam Reynolds <sreyn@proton.me>
2024-03-07 15:36:23 -0800

    use lazyvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,2 @@
-local vim = vim
-local Plug = vim.fn['plug#']
-
-vim.wo.number = true
-
-vim.call('plug#begin')
-
-Plug('nvim-lua/plenary.nvim')
-Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.5' })
-
-vim.call('plug#end')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
